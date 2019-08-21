d6b5b3c3cff3d519de50263bdb3ed635c864646c
AnnaStulen <54374216+AnnaStulen@users.noreply.github.com>
2019-08-21 21:49:57 +0300

    delete Add method

--- ConsoleApp1/Class14.cs
+++ ConsoleApp1/Class14.cs
@@ -5,18 +5,6 @@ namespace ConsoleApp1
 {
     public class Class14
     {
-        public int Add(int a, int b)
-        {
-            var qa = a + b;
-            Console.WriteLine($"{qa}");
-            return qa;
-        }
-
-        public void Add(int a, int b, int c)
-        {
-            var qw = a + b + c;
-            Console.WriteLine(qw);
-        }
 
         public int Summq(int a, int b)
         {
